b10d39d4096984a45de1625d149e6124c73056bd
Irfhan Ardhiansyah <irfhanardhiansyah0@gmail.com>
2026-09-24 12:02:48 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle?.addEventListener('click', () => {
+	const isOpen = navMenu.classList.toggle('is-open');
+	menuToggle.setAttribute('aria-expanded', String(isOpen));
+});
+
+navMenu?.querySelectorAll('a').forEach((link) => {
+	link.addEventListener('click', () => {
+		navMenu.classList.remove('is-open');
+		menuToggle?.setAttribute('aria-expanded', 'false');
+	});
+});
